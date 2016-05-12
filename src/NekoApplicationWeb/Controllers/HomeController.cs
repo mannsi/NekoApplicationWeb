5bfaa524c1a68a980f1a861d4282b7c25063cf98
Guðbjörn Einarsson <Gubjrn Einarsson>
2016-05-12 20:36:33 +0000

    Original commit

---
+++ src/NekoApplicationWeb/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace NekoApplicationWeb.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewData["ContentHeader"] = "Umsókn um Neko lán";
+            ViewData["selectedNavPillId"] = "navPillApplicant";
+            return View();
+        }
+
+        [Route("Menntun")]
+        public IActionResult Education()
+        {
+            ViewData["ContentHeader"] = "Umsókn um Neko lán";
+            ViewData["selectedNavPillId"] = "navPillEducation";
+            return View();
+        }
+
+        [Route("Starfsferill")]
+        public IActionResult Employment()
+        {
+            ViewData["ContentHeader"] = "Umsókn um Neko lán";
+            ViewData["selectedNavPillId"] = "navPillEmployment";
+            return View();
+        }
+
+        [Route("Fjarmal")]
+        public IActionResult Finances()
+        {
+            ViewData["ContentHeader"] = "Umsókn um Neko lán";
+            ViewData["selectedNavPillId"] = "navPillFinances";
+            return View();
+        }
+
+        [Route("Lan")]
+        public IActionResult Loan()
+        {
+            ViewData["ContentHeader"] = "Umsókn um Neko lán";
+            ViewData["selectedNavPillId"] = "navPillLoan";
+            return View();
+        }
+
+        [Route("Samatekt")]
+        public IActionResult Summary()
+        {
+            ViewData["ContentHeader"] = "Umsókn um Neko lán";
+            ViewData["selectedNavPillId"] = "navPillSummary";
+            return View();
+        }
+
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/NekoApplicationWeb/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Write your Javascript code.
+$(document)
+    .ready(function () {
+        var selectedNavPillId = $("#selectedNavPillId").attr("selectedNavPillId");
+        $(".nav-pills>li")
+            .each(function() {
+                var pillId = $(this).attr("id");
+                if (pillId === selectedNavPillId) {
+                    $(this).addClass("active");
+                }
+            });
+    });
+
