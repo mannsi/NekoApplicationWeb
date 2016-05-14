4f1c842aa59329157adff86e59daf43d59e70423
gudbj <gudbj@DESKTOP-R18KPS7>
2016-05-14 11:19:20 +0000

    Dynamic pill progress bar

--- src/NekoApplicationWeb/wwwroot/js/site.js
+++ src/NekoApplicationWeb/wwwroot/js/site.js
@@ -1,13 +1,27 @@
 ﻿// Write your Javascript code.
 $(document)
     .ready(function () {
-        var selectedNavPillId = $("#selectedNavPillId").attr("selectedNavPillId");
-        $(".nav-pills>li")
-            .each(function() {
-                var pillId = $(this).attr("id");
-                if (pillId === selectedNavPillId) {
-                    $(this).addClass("active");
-                }
-            });
+        setActivePill();
+        updateProgressBar();
     });
 
+function setActivePill() {
+    var selectedNavPillId = $("#selectedNavPillId").attr("selectedNavPillId");
+    $(".nav-pills>li")
+        .each(function () {
+            var pillId = $(this).attr("id");
+            if (pillId === selectedNavPillId) {
+                $(this).addClass("active");
+            }
+        });
+};
+
+function updateProgressBar() {
+    var numberOfCompleted = $(".nav-pills>li>a>i.fa-check-circle").length;
+    var numberOfNonCompleted = $(".nav-pills>li>a>i.fa-minus-circle").length;
+    var total = numberOfCompleted + numberOfNonCompleted;
+    var percentageString = (numberOfCompleted/total * 100) + "%";
+
+    $("#pillProgressBar").css("width", percentageString);
+    $("#pillProgressBar").text(percentageString);
+};
