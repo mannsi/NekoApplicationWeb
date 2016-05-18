3c737e1194bebc48c95f2b701861d1a80aa4f5cc
gudbj <gudbj@DESKTOP-R18KPS7>
2016-05-18 21:42:37 +0000

    Added database support

--- src/NekoApplicationWeb/Startup.cs
+++ src/NekoApplicationWeb/Startup.cs
@@ -5,17 +5,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NekoApplicationWeb.Models;
 using NekoApplicationWeb.Services;
+using Microsoft.Data.Entity;
 
 namespace NekoApplicationWeb
 {
     public class Startup
     {
+        readonly bool _isDevelopment;
+
         public Startup(IHostingEnvironment env)
         {
+            _isDevelopment = env.IsDevelopment();
+
             // Set up configuration sources.
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
@@ -28,6 +35,36 @@ namespace NekoApplicationWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString;
+            if (_isDevelopment)
+            {
+                connectionString = Configuration["NekoData:DebugConnection:ConnectionString"];
+            }
+            else
+            {
+                connectionString = Configuration["Data:DefaultConnection:ConnectionString"];
+            }
+
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlServer(connectionString);
+                });
+
+            // Add Identity services to the services container.
+            services.AddIdentity<ApplicationUser, IdentityRole>(o =>
+            {
+                o.Password.RequireDigit = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireNonLetterOrDigit = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequiredLength = 6;
+                o.Cookies.ApplicationCookie.LoginPath = "/Account/StartPage";
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             // Add framework services.
             services.AddMvc();
 
@@ -54,7 +91,7 @@ namespace NekoApplicationWeb
             app.UseIISPlatformHandler();
 
             app.UseStaticFiles();
-
+            app.UseIdentity(); // This allows asp login
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
