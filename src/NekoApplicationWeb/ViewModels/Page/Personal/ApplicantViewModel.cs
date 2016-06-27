ef9428739d641b8a0aa7496a929fe0319c79ca8a
gudbj <gudbj@DESKTOP-R18KPS7>
2016-06-27 10:50:59 +0000

    Set Twitter and LinkedIn paths to personal page

--- src/NekoApplicationWeb/ViewModels/Page/Personal/ApplicantViewModel.cs
+++ src/NekoApplicationWeb/ViewModels/Page/Personal/ApplicantViewModel.cs
@@ -5,22 +5,24 @@ namespace NekoApplicationWeb.ViewModels.Page.Personal
     public class ApplicantViewModel
     {
         [Required(ErrorMessage = "Verður að fylla")]
-        [Display(Name = "Kennitala")]
         [DataType(DataType.Text)]
         public string Ssn { get; set; }
 
         [Required(ErrorMessage = "Verður að fylla")]
         [DataType(DataType.Text)]
-        [Display(Name = "Nafn")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Verður að fylla")]
         [DataType(DataType.EmailAddress, ErrorMessage = "Verður að vera löglegt netfang")]
-        [Display(Name = "Netfang")]
         public string Email { get; set; }
 
         [DataType(DataType.Url, ErrorMessage = "Ólögleg slóð")]
-        [Display(Name = "Facebook slóð")]
         public string FacebookPath { get; set; }
+
+        [DataType(DataType.Url, ErrorMessage = "Ólögleg slóð")]
+        public string TwitterPath { get; set; }
+
+        [DataType(DataType.Url, ErrorMessage = "Ólögleg slóð")]
+        public string LinkedInPath { get; set; }
     }
 }
